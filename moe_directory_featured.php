612aca7ba52c33599ed0f804746b7f8a528afe05
Moe Loubani <moe@loubani.com>
2013-05-06 01:49:39 -0400

    Added featured listing limiter
    Added featured listing limiting so that only some packages can have a
    featured listing option.

--- moe_directory_featured.php
+++ moe_directory_featured.php
@@ -8,7 +8,107 @@ Author: Moe Loubani
 Author URI: http://www.moeloubani.com
 */
 
+/* THEME OPTIONS PAGE ADD */
+
+//include the main class file
+	require_once("admin-page-class/admin-page-class.php");
+	
+	
+	/**
+	 * configure your admin page
+	 */
+	$config = array(        
+		'menu'             => 'settings',
+		'page_title'       => __('Featured Listing Settings','moe_featured_td'),
+		'capability'       => 'edit_themes',
+		'option_group'     => 'moe_featured_settings', 
+		'id'               => 'moe_featured_settings_admin',
+		'fields'           => array(),
+		'local_images'     => false,
+		'use_with_theme'   => false
+	);    
+	
+	/**
+	 * Initiate your admin page
+	 */
+	$moe_options_panel = new BF_Admin_Page_Class($config);
+	$moe_options_panel->OpenTabs_container('');
+	
+	/**
+	 * define your admin page tabs listing
+	 */
+	$moe_options_panel->TabsListing(array(
+		'links' => array(
+		'options_1' =>    __('Options','moe_featured_td'),
+		)
+	));
+	
+	/**
+	 * Open admin page first tab
+	 */
+	$moe_options_panel->OpenTab('options_1');
+
+	/*Add fields admin page */
+
+	//title
+	$moe_options_panel->Title(__("Featured Listings Options","moe_featured_td"));
+	//Roles checkbox field
+	$moe_options_panel->addRoles('moe_roles_id',array('type' => 'checkbox_list' ),array('name'=> __('Allowed Packages','moe_featured_td'), 'desc' => __('Turn on the roles you would like to enable featured for.','moe_featured_td')));
+
+	$moe_options_panel->CloseTab();
+
+function moe_get_current_role() {
+	$moe_user_current_role = wp_get_current_user();
+	$moe_plugin_data = get_option('moe_featured_settings');
+
+	$saved_roles = $moe_plugin_data['moe_roles_id'];
+
+	if (!function_exists('moe_get_editable_roles')) {
+		function moe_get_editable_roles() {
+			global $wp_roles;
+			if ( ! isset( $wp_roles ) ) {
+				$wp_roles = new WP_Roles();
+			}
+		
+
+		$all_roles = $wp_roles->get_names();
+		$editable_roles = apply_filters('editable_roles', $all_roles);
+
+		return $editable_roles;
+		}
+
+		$moe_current_roles = moe_get_editable_roles();
+
+		foreach ($moe_current_roles as $moe_rolename => $moe_roleslug) {
+			foreach($saved_roles as $saved_role) {
+				if ($saved_role == $moe_roleslug) {
+					if ($moe_user_current_role->roles[0] == $moe_rolename) {
+						add_action( 'post_submitbox_misc_actions', 'moe_featured_meta_cb' );
+						break 2;
+					}
+				}
+		  	}
+		}
+}
+}
+
+add_action('plugins_loaded', 'moe_get_current_role');
+
+
+// Enqueue scripts
+
+function moe_add_js_feat() {
+	wp_enqueue_script(
+		'moe_featu_plug',
+		plugins_url( '/js/script.js' , __FILE__ ),
+		array( 'jquery' )
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'moe_add_js_feat' );
+
 // Add meta box in post submit area
+
 function moe_featured_meta_cb( $post )
 {
   global $post;
@@ -19,16 +119,15 @@ function moe_featured_meta_cb( $post )
 		?>
 		<div class="misc-pub-section">
 			<input type="checkbox" name="moe_featured_checkbox" id="moe_featured_checkbox" <?php checked( $check, 'on' ); ?> />
-			<label for="moe_featured_checkbox">Check for featured item.</label>
+			<label for="moe_featured_checkbox"><?php _e("Check for featured item.", 'moe_featured_td');?></label>
 		</div>
-		<?php	
+		<?php   
 	}
 }
 
-add_action( 'post_submitbox_misc_actions', 'moe_featured_meta_cb' );
-
 
 // Save the check box
+
 function moe_meta_box_save( $post_id )
 {
 	// Bail if we're doing an auto save
@@ -53,21 +152,11 @@ add_action( 'save_post', 'moe_meta_box_save' );
 function templateRedirect()
 {
 	if (isset($_GET['dir-search'])) {
-    $search_template = dirname( __FILE__ ) . '/themefiles/search.php';
-    include $search_template;
-    exit;
+	$search_template = dirname( __FILE__ ) . '/themefiles/search.php';
+	include $search_template;
+	exit;
 	}
 }
  
 // add our function to template_redirect hook
 add_action('template_redirect', 'templateRedirect');
-
-function moe_add_js_feat() {
-	wp_enqueue_script(
-		'moe_featu_plug',
-		plugins_url( '/js/script.js' , __FILE__ ),
-		array( 'jquery' )
-	);
-}
-
-add_action( 'wp_enqueue_scripts', 'moe_add_js_feat' );
